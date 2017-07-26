189fc23d44ac857818d511a901fcf4fb7613c05c
许伟鹏 <728684457@qq.com>
2017-07-26 22:39:16 +0800

    初始化项目

---
+++ mytest/mytest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  mytest
+//
+//  Created by 许伟鹏 on 2017/7/26.
+//  Copyright © 2017年 xwp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ mytest/mytest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  mytest
+//
+//  Created by 许伟鹏 on 2017/7/26.
+//  Copyright © 2017年 xwp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
